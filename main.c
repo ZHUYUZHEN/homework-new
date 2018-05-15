39c1af0a0e6906e5accf18dfafa11df6556aac08
janeju <janeju0712@gmail.com>
2018-05-15 18:39:41 +0800

    add

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int d,s;
-	for(d=0,s=0;d<=100;++d)
-	s=s+d;
-	printf("s=%d",s);
-	return 0;
+	int data,sum; //宣告變數 
+	for(data=0,sum=0;data<=100;++data) //設定變數範圍，且變數每次增加1 
+	sum=sum+data;  //變數sum從1加到100 
+	printf("sum=%d",sum);  //印出變數的值 
+	return 0;   
 }
